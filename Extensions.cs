3fd2bc0fb6447498aea1fa5a6b70c8c5354e93b9
Muhammed Kara <47905966+svndevelopment@users.noreply.github.com>
2019-02-23 02:04:31 +0100

    1.0.0.0

---
+++ Extensions.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace SVN.Wpf
+{
+    public static class Extensions
+    {
+        public static BitmapImage ToBitmapImage(this byte[] param)
+        {
+            if (param is null || param.Length == default(long))
+            {
+                return null;
+            }
+
+            using (var stream = new MemoryStream(param))
+            {
+                var image = new BitmapImage();
+                image.BeginInit();
+
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.UriSource = null;
+                image.StreamSource = stream;
+
+                image.EndInit();
+                image.Freeze();
+
+                return image;
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SVN.Wpf")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SVN Development")]
+[assembly: AssemblyProduct("SVN.Wpf")]
+[assembly: AssemblyCopyright("Copyright © Muhammed Kara 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("ec36b966-dcc8-4e19-bc63-d81c900fc0d7")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
